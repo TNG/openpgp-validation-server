c2f379cccd053f20ed9280b84993e1779e241157
Martin Altmayer <martin.altmayer@tngtech.com>
2016-05-03 08:12:42 +0200

    Implemented a simple parser for multipart/mixed messages.

--- email-server/server.go
+++ email-server/server.go
@@ -2,23 +2,109 @@ package emailserver
 
 import (
 	"log"
-
+	"net"
+	"mime"
+	"bytes"
+	"io"
+	"io/ioutil"
+	"net/mail"
+	"mime/multipart"
 	"github.com/mhale/smtpd"
+	"errors"
+	"fmt"
 )
+
+type Mail struct {
+	FromAddress string
+	ToAddresses []string
+	Subject string
+	Text string
+	Attachments [][]byte
+}
+
+type Handler func(mail *Mail)
+type Parser func(message *mail.Message) (*Mail, error)
 
 // MailServer contains the information necessary to run
 // a server which receives and handles mail.
 type MailServer struct {
 	Address string
-	Handler smtpd.Handler
+	Handler Handler
+	Parser Parser
 }
 
 // Create returns a MailServer struct given a listening address and a mail handler.
-func Create(Address string, Handler smtpd.Handler) *MailServer {
-	return &MailServer{Address, Handler}
+func Create(Address string, Handler Handler) *MailServer {
+	return &MailServer{Address, Handler, parseMail}
+}
+
+func (server *MailServer) mailHandler(origin net.Addr, fromAddress string, toAddresses []string, data []byte) {
+	message, err := mail.ReadMessage(bytes.NewReader(data))
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	mail, err := server.Parser(message)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	mail.FromAddress = fromAddress
+	mail.ToAddresses = toAddresses
+	server.Handler(mail)
+}
+
+func parseMail(message *mail.Message) (*Mail, error) {
+	subject := message.Header.Get("Subject")
+	contentType := message.Header.Get("Content-Type")
+	contentType, params, err := mime.ParseMediaType(contentType)
+	if err != nil {
+		return nil, err
+	}
+	if contentType == "multipart/mixed" {
+		mail, err := parseMultipartMixedMessage(message.Body, params)
+		if err != nil {
+			return nil, err
+		}
+		mail.Subject = subject
+		return mail, nil
+	}
+	return nil, fmt.Errorf("Unknown mail content type: %s", contentType)
+}
+
+func parseMultipartMixedMessage(messageBody io.Reader, params map[string]string) (*Mail, error) {
+	boundary, ok := params["boundary"]
+	if !ok {
+		return nil, errors.New("multipart/mixed mail without boundary")
+	}
+
+	reader := multipart.NewReader(messageBody, boundary)
+	mail := &Mail{}
+	for {
+		part, err := reader.NextPart()
+		if err != nil {
+			if err == io.EOF {
+				return mail, nil
+			}
+			return nil, err
+		}
+		contentType := part.Header.Get("Content-Type")
+		switch contentType {
+		case "text/plain":
+			text, err := ioutil.ReadAll(part)
+			if err != nil {
+				return nil, err
+			}
+			mail.Text += string(text)
+		case "":
+			return nil, errors.New("No Content-Type found in multipart message part")
+		default:
+			return nil, errors.New("Unknown Content-Type (%s) found in multipart message part")
+		}
+	};
 }
 
 // Run starts a goroutine which listens and processes mail as it arrives.
 func (server *MailServer) Run() {
-	go log.Fatal(smtpd.ListenAndServe(server.Address, server.Handler, "gpg-validation-server", ""))
+	go log.Fatal(smtpd.ListenAndServe(server.Address, server.mailHandler, "gpg-validation-server", ""))
 }
--- email-server/server_test.go
+++ email-server/server_test.go
@@ -1,41 +1,115 @@
 package emailserver
 
 import (
-	"bytes"
-	"fmt"
-	"log"
-	"net"
-	"net/mail"
 	"testing"
+	"fmt"
 	"time"
-
-	"github.com/TNG/gpg-validation-server/email-client"
+	"net/mail"
+	"gpg-validation-server/email-client"
+	"strings"
 )
 
 var receiveChan = make(chan string)
 
 func init() {
-	server := Create("127.0.0.1:2525", mailHandler)
+	server := Create("127.0.0.1:2525", mailTestHandler)
+	fakeParser := func (message *mail.Message) (*Mail, error) {
+		return &Mail{"", nil, "QWERTYIOP", "", nil}, nil
+	}
+	server.Parser = fakeParser
 	go server.Run()
 	time.Sleep(1 * time.Millisecond)
 }
 
-func mailHandler(origin net.Addr, from string, to []string, data []byte) {
-	_, err := mail.ReadMessage(bytes.NewReader(data))
-	if err != nil {
-		log.Fatal(err)
-	}
-	receiveChan <- fmt.Sprintf("%s -> %s", from, to[0])
+func mailTestHandler(mail *Mail) {
+	receiveChan <- mail.FromAddress
+	receiveChan <- mail.ToAddresses[0]
+	receiveChan <- mail.Subject
 }
 
 func TestReceiveMail(t *testing.T) {
-	expected := "ray@tomlinson.net -> ray.tomlinson@mail.org"
-	err := emailclient.SendMail("ray@tomlinson.net", "ray.tomlinson@mail.org", "Subject: QWERTYIOP\n\nBody")
-	if err != nil {
-		log.Fatal(err)
+	expectedFromAddress := "ray@tomlinson.net"
+	expectedToAddress := "ray.tomlinson@mail.org"
+	expectedSubject := "QWERTYIOP"
+	_ = emailclient.SendMail(expectedFromAddress, expectedToAddress, "\n\n")
+	receivedFromAddress := <-receiveChan
+	receivedToAddress := <-receiveChan
+	receivedSubject := <-receiveChan
+
+	if receivedFromAddress != expectedFromAddress {
+		t.Error("Expected:", expectedFromAddress, " Received:", receivedFromAddress)
 	}
-	received := <-receiveChan
-	if received != expected {
-		t.Error("Expected:", expected, " Received:", received)
+
+	if receivedToAddress != expectedToAddress {
+		t.Error("Expected:", expectedToAddress, " Received:", receivedToAddress)
+	}
+
+	if receivedSubject != expectedSubject {
+		t.Error("Expected:", expectedSubject, " Received:", receivedSubject)
+	}
+}
+
+func parseMailFromString(source string) (*Mail, error) {
+	reader := strings.NewReader(source)
+	message, err := mail.ReadMessage(reader)
+	if err != nil {
+		return nil, err
+	}
+	return parseMail(message)
+}
+
+func createMailString(subject, contentType, text string) string {
+	lines := []string{
+		fmt.Sprintf("Subject: %s", subject),
+		fmt.Sprintf("Content-Type: %s", contentType),
+		"",
+		"--f",
+		"Content-Type: text/plain",
+		"",
+		text,
+		"--f--",
+		""}
+	return strings.Join(lines, "\r\n")
+}
+
+func TestParseMailSubject(t *testing.T) {
+	mail, err := parseMailFromString(createMailString("Test", "multipart/mixed; boundary=\"f\"", "text"))
+	if err != nil {
+		t.Error("Error while parsing mail subject:", err)
+	}
+	if mail.Subject != "Test" {
+		t.Error("Expected subject 'Test', got", mail.Subject)
+	}
+}
+
+func TestParseMailInvalidContentType(t *testing.T) {
+	mail, err := parseMailFromString(createMailString("S", "blah", ""))
+	if err == nil {
+		t.Error("Expected an error when parsing mail with invalid content type")
+	}
+	if mail != nil {
+		t.Error("Must not parse mail with invalid content type")
+	}
+}
+
+func TestParseMailWithoutBoundaryString(t *testing.T) {
+	mail, err := parseMailFromString(createMailString("S", "multipart/mixed;", ""))
+	if err == nil {
+		t.Error("Expected an error when parsing mail without boundary string")
+	}
+	if mail != nil {
+		t.Error("Must not parse mail without boundary string")
+	}
+}
+
+func TestParseMailMessage(t *testing.T) {
+	mail, err := parseMailFromString(createMailString(
+		"S", "multipart/mixed; boundary=\"f\"", "Hello there!"))
+
+	if err != nil {
+		t.Error("Error while parsing a multipart mail:", err)
+	}
+	if mail.Text != "Hello there!" {
+		t.Errorf("Expected message text 'Hello there!', got '%s'", mail.Text)
 	}
 }

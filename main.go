53cb9e21c0cb836ca0975085d5e2fb4653f36104
Patrick Harböck <Pharb@users.noreply.github.com>
2016-05-14 00:21:59 +0200

    Add "process-mail" CLI command (resolves #8)

--- main.go
+++ main.go
@@ -1,7 +1,88 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/TNG/gpg-validation-server/mail"
+	"github.com/codegangsta/cli"
+	"io"
+	"log"
+	"os"
+)
+
+const errorExitCode = 1
+
+func appAction(c *cli.Context) error {
+	fmt.Println("Args", c.Args())
+	fmt.Println("host", c.String("host"))
+	// TODO #11 Start the servers
+	return nil
+}
+
+func processMailAction(c *cli.Context) error {
+	var err error
+	var input io.Reader
+
+	file := c.String("file")
+
+	if file == "" {
+		input = os.Stdin
+	} else {
+		input, err = os.Open(file)
+		if err != nil {
+			return err
+		}
+	}
+
+	entity, _ := mail.ParseMail(input)
+	log.Println(entity)
+
+	return nil
+}
+
+func cliErrorHandler(action func(*cli.Context) error) func(*cli.Context) cli.ExitCoder {
+	return func(c *cli.Context) cli.ExitCoder {
+		if err := action(c); err != nil {
+			return cli.NewExitError(fmt.Sprint("Error: ", err), errorExitCode)
+		}
+		return nil
+	}
+}
+
+func runApp(args []string) {
+	app := cli.NewApp()
+	app.Name = "GPG Validation Service"
+	app.Usage = "Run a server that manages email verification and signs verified keys with the servers GPG key."
+
+	app.Commands = []cli.Command{
+		{
+			Name:   "process-mail",
+			Usage:  "process an incoming email",
+			Action: cliErrorHandler(processMailAction),
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "file",
+					Value: "",
+					Usage: "`FILE_PATH` of the mail file, omit to read from stdin ",
+				},
+			},
+		},
+	}
+
+	app.Action = cliErrorHandler(appAction)
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "localhost",
+			Usage: "`HOST` of the mail server",
+		},
+	}
+
+	err := app.Run(args)
+	if err != nil {
+		cli.OsExiter(errorExitCode)
+	}
+}
 
 func main() {
-	fmt.Println("gpg-validation-server")
+	runApp(os.Args)
 }
---
+++ main_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/codegangsta/cli"
+	"testing"
+)
+
+const okExitCode = 0
+
+func getMockOsExiter(t *testing.T, expectedExitCode int) func(int) {
+	return func(code int) {
+		if code != expectedExitCode {
+			t.Fatalf("Unexpected exit code: expected %d, got %d", expectedExitCode, code)
+		}
+	}
+}
+
+func testMainWithArguments(t *testing.T, expectedExitCode int, args ...string) {
+	cli.OsExiter = getMockOsExiter(t, expectedExitCode)
+
+	appArgs := append([]string{"name-of-binary"}, args...)
+	runApp(appArgs)
+}
+
+func TestRunMain(t *testing.T) {
+	testMainWithArguments(t, okExitCode)
+}
+
+func TestProcessMailDefault(t *testing.T) {
+	testMainWithArguments(t, okExitCode, "process-mail")
+}
+
+func TestProcessMailFile(t *testing.T) {
+	testMainWithArguments(t, okExitCode, "process-mail", "--file", "./test-mails/plaintext.eml")
+}
+
+func TestProcessFileError(t *testing.T) {
+	testMainWithArguments(t, errorExitCode, "process-mail", "--file", "./invalid")
+}

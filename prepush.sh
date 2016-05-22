885ec90b20f329461621aa44431fbd93f7f9c6c5
Patrick Harböck <Pharb@users.noreply.github.com>
2016-05-22 17:56:33 +0200

    Add prepush.sh and minor build scripts cleanup.

---
+++ prepush.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+STATUS=0
+PIDS=""
+
+./enforce_gofmt.sh &
+PIDS+="$! "
+
+go test -covermode=atomic ./... &  # Prefer short, human readable output of test results here
+PIDS+="$! "
+
+./lint.sh &
+PIDS+="$! "
+
+for pid in $PIDS; do
+    wait $pid
+    if [ $? -ne 0 ]; then
+        STATUS=$?
+    fi
+done
+
+exit $STATUS
